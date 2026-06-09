458fb8553361e3923139e3104125ddddecf5fecf
l1m120 <159098445+l1m120@users.noreply.github.com>
2026-06-09 16:41:36 +0800

    Refined Image Directory

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -254,7 +254,7 @@ export default function Home() {
       images: [
         "/images/IMG-20260407-WA0021.jpg",
         "/images/IMG-20260407-WA0022.jpg",
-        "/images/room 3 amended.jpeg",
+        "/images/room 3 amended.png",
         "/images/IMG-20260407-WA0028.jpg",
         "/images/IMG-20260407-WA0029.jpg"
       ],
